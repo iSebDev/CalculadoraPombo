2cdca04d76adb661cdf93102e73750d930ff441c
Seb <75587701+iSebDev@users.noreply.github.com>
2025-04-04 20:17:12 -0300

    Megaproyecto fisica pombomaster 50 mil

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const materiales = {
-        "": ""
-    }
+    const materiales = ["Hielo", "Vidrio", "Madera", "Acero", "Cuero"];
+
+    const comboBoxes = document.querySelectorAll(".materials");
+
+    comboBoxes.forEach((c) => {
+        let i = 0;
+        materiales.forEach((m) => {
+            const opt = document.createElement("option");
+            opt.innerHTML = m;
+            if (i === 0) {
+                opt.setAttribute("selected", "");
+            }
+            opt.setAttribute("value", i)
+            i++;
+
+            c.appendChild(opt);
+        });
+    });
 });
