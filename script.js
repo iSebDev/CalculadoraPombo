29b87c6ebcf3f7d5f6c965b49894d8e45460406b
Seb <75587701+iSebDev@users.noreply.github.com>
2025-04-04 20:40:45 -0300

    Pancho

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const materiales = {
-        "": ""
+    const materiales = ["Hielo", "Vidrio", "Madera", "Acero", "Cuero"];
+
+    const comboBoxes = document.querySelectorAll(".materials");
+
+    const coefs = {
+        "0-0": 0.1,
     }
+
+    const coefRoz = (m1, m2) => {
+
+        const mat = `${m1}-${m2}`;
+
+        return coefs[mat];
+
+    }
+
+    comboBoxes.forEach((c) => {
+        let i = 0;
+        materiales.forEach((m) => {
+            const opt = document.createElement("option");
+            opt.innerHTML = m;
+            if (i === 0) {
+                opt.setAttribute("selected", "");
+            }
+            opt.setAttribute("value", i)
+            i++;
+
+            c.appendChild(opt);
+        });
+    });
+
+    
 });
